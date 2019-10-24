772be0ab50e510510cd09d3f607560bf5f7da017
Jiesi Ren <jiesiren@gmail.com>
2019-10-24 15:33:19 -0700

    Add up to ex 1-12

---
+++ 1-12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define TRUE (1==1)
+#define FALSE (!TRUE)
+
+int main() {
+    int c, prev_is_ws;
+
+    prev_is_ws = TRUE;
+
+    while ((c = getchar()) != EOF) {
+        // Assuming these are the "whitespace" separators we are concerned about
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (!prev_is_ws) {
+                putchar('\n');
+            }
+            prev_is_ws = TRUE;
+        } else {
+            putchar(c);
+            prev_is_ws = FALSE;
+        }
+    }
+}
